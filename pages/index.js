70c503bd1c6e7eb1e9a75cd6a42f3d6c85da104f
William <wlouislouisy@gmail.com>
2022-01-19 23:19:03 +0100

    Loading component created

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Card from "@components/Card";
+import Loading from "@components/Loading";
 import WelcomeCard from "@components/WelcomeCard";
 import Image from "next/image";
 
 export default function IndexPage() {
   return (
     <div className="flex flex-col items-center justify-center space-y-12">
-      <WelcomeCard question="Would you like a personalized support?" />
+      <Loading />
     </div>
   );
 }
