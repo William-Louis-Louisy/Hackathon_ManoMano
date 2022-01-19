0749c49709de88c843ccf343c5786401cb6c617f
William <wlouislouisy@gmail.com>
2022-01-20 00:19:27 +0100

    Card Shopping and Product card created

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Card from "@components/Card";
+import CardShopping from "@components/CardShopping";
 import Loading from "@components/Loading";
 import WelcomeCard from "@components/WelcomeCard";
 import Image from "next/image";
@@ -6,7 +7,7 @@ import Image from "next/image";
 export default function IndexPage() {
   return (
     <div className="flex flex-col items-center justify-center space-y-12">
-      <Loading />
+      <CardShopping question="Which product do you want?" />
     </div>
   );
 }
