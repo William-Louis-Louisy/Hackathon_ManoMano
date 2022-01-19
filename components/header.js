483650825ce619ef55fc94489ab60d459f303063
Dr Wouse <e.gon66@outlook.fr>
2022-01-19 20:57:37 +0100

    Add Icon in header

--- components/header.js
+++ components/header.js
@@ -1,21 +1,21 @@
 import Link from "next/link";
 import Image from "next/image";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export default function Header() {
   return (
     <header className="bg-white">
       <div className="flex flex-wrap items-center justify-between lg:container mx-auto md:flex-no-wrap md:px-6">
-        <div className="flex items-center">
-          <Link passHref href="https://www.manomano.fr/">
-            <Image
-              src="/mano.png"
-              width={175}
-              height={55}
-              priority
-              alt="ManoMano logo"
-            />
-          </Link>
-        </div>
+        <Link passHref href="https://www.manomano.fr/">
+          <Image
+            src="/mano.png"
+            width={175}
+            height={55}
+            priority
+            alt="ManoMano logo"
+          />
+        </Link>
+        <ShoppingCartIcon sx={{ fontSize: 35 }} className="mr-3"/>
       </div>
     </header>
   );
