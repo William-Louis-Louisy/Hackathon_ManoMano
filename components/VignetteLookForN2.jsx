50b49e8a5833e8776847519a9d597c94d0b48cfa
Dr Wouse <e.gon66@outlook.fr>
2022-01-20 18:00:08 +0100

    Add component CardLvl | VignetteAvanced | VignetteNoob

--- components/VignetteLookForN2.jsx
+++ components/VignetteLookForN2.jsx
@@ -8,7 +8,7 @@ const VignetteLookForN2 = () => {
       <div className="vignette my-2 pb-2 pt-4 flex flex-col justify-between items-center rounded-xl">
         <img src="/charpentier.png" alt="Category image" className="w-3/5" />
         <h1 className="font font-bold pb-2 text-center flex flex-col mt-1">
-          Works
+          WORKS
         </h1>
       </div>
     </motion.button>
---
+++ components/VignetteNoob.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import { motion } from "framer-motion";
+
+const VignetteNoob = () => {
+  return (
+    <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+      <div
+        className="vignette my-2 pb-2 pt-4 flex flex-col justify-between items-center rounded-xl"
+        onClick={() => setIsClicked((isClicked) => !isClicked)}
+      >
+        <img
+          src="/ajouter-au-panier.png"
+          alt="Category image"
+          className="w-3/5"
+        />
+        <h1 className="font font-bold pb-2 text-center flex flex-col mt-1">
+          BEGINNER
+        </h1>
+      </div>
+    </motion.button>
+  );
+};
+
+export default VignetteNoob;
