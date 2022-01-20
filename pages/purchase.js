cedd9c17740e1f917ffad0bdb1cdb7c8b29cfaa4
Dr Wouse <e.gon66@outlook.fr>
2022-01-20 18:28:40 +0100

    Rénitialistiont page purchase

--- pages/purchase.js
+++ pages/purchase.js
@@ -1,10 +1,10 @@
-import CardLvl from "@components/CardLvl";
+import CardPurchase from "@components/CardPurchase";
 import React from "react";
 
 const purchase = () => {
   return (
     <div className="flex flex-col items-center justify-center space-y-12">
-      <CardLvl />
+      <CardPurchase />
     </div>
   );
 };
